229ad3ada505f2d50b7eaa0f8705c39488d714ed
Timbino <timridderhof@gmail.com>
2026-01-10 17:34:35 +0100

    styling

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -57,14 +57,24 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Welcome") }} {{ $user->name }} <br/>
+                    {{ __("Welcome back") }} {{ $user->name }} <br/>
                     {{ __("Health") }} {{ $user->health }} <br/>
                     {{ __("Experience") }} {{ $user->exp }} <br/>
-                    {{ __("Money") }} {{ $user->money }} <br/>
-                    @if ($user->exp <= 49)
-                        Nobody
-                    @elseif ($user->exp < 100 and $user->exp > 49)
-                        Bosozoku                
+                    {{ __("Money") }} ¥{{ number_format($user->money) }} <br/>
+                    @if ($user->exp >= 0 and $user->exp < 99)
+                        Rank: <b>Nobody</b>
+                    @elseif ($user->exp < 250)
+                        Rank: <b>Bosozoku</b>           
+                    @elseif ($user->exp < 500)
+                        Rank: <b>Shatei</b> 
+                    @elseif ($user->exp < 1000)
+                        Rank: <b>Kyodai</b> 
+                    @elseif ($user->exp < 2000)
+                        Rank: <b>Shateeigashira</b> 
+                    @elseif ($user->exp < 5000)
+                        Rank: <b>Wakagashira</b> 
+                    @elseif ($user->exp > 5000)
+                        Rank: <b>Oyabun</b> 
                     @endif
                     
                 </div>                
@@ -74,40 +84,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Inventory") }}  <br/>
+                    <b>{{ __("Inventory") }}  </b><br/>
                     @foreach ($weapons as $weapon)
                         <p>{{ $weapon->name }}  </p>
                     @endforeach      
                 </div>                
             </div>
             <br>
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <!-- <img src="{{ asset('images/japan.svg') }}" alt="Your SVG"> -->
-                    {!! file_get_contents(public_path('images/japan.svg')) !!}
-                    <!-- <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                    </svg>     -->
-                    <svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"></svg>
-                </div>                
-            </div>
         </div>
     </div>
-    <div class="dropdown">
-        <button class="dropbtn">Dropdown</button>
-        <div class="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-        </div>
-    </div>
-    <script>
-        document.getElementsByClassName('test').addEventListener('mouseenter', function() {
-            this.setAttribute('fill', 'red');
-        });
-
-        document.getElementsByClassName('test').addEventListener('mouseleave', function() {
-            this.setAttribute('fill', 'blue');
-        });
-    </script>
 </x-app-layout>
